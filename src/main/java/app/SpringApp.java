ca36be282a7a85e81a035ac9c2b88d1081195c41
RobertKrzywina <robert.krzywina@gmail.com>
2018-10-14 22:46:58 +0200

    added code to output

--- src/main/java/app/SpringApp.java
+++ src/main/java/app/SpringApp.java
@@ -8,10 +8,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import java.util.Random;
+
 @Configuration
 @ComponentScan(basePackageClasses = Producer.class)
 @EnableAspectJAutoProxy
 public class SpringApp {
+
+    private static Random random = new Random();
+
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx =
                 new AnnotationConfigApplicationContext(SpringApp.class);
@@ -23,6 +28,11 @@ public class SpringApp {
         producer.createBankAccountForUser(new User("Mia"), new BankAccount("Mia"));
         producer.createBankAccountForUser(new User("Tim"), new BankAccount("Tim"));
         producer.createBankAccountForUser(new User("Joe"), new BankAccount("Joe"));
+
+        for (int i=0; i<6; i++) {
+            double randomCash = random.nextDouble();
+            producer.addMoney(i, randomCash);
+        }
 
         producer.addMoney(2, 12120.1);
         producer.addMoney(4, 1212.43);
